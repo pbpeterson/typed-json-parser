eb1cd64ff6b05a69ba131e4d26cc8dba0abb2b03
Guilherme Saliba <salibagui19@gmail.com>
2025-07-21 00:03:59 -0300

    feat(error): include error on message when instantiating SyntaxError

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,31 @@
 type ParseResult<T = any> =
-  | {
-      data: T;
-      error: null;
-    }
-  | {
-      data: null;
-      error: Error;
-    };
+	| {
+			data: T;
+			error: null;
+	  }
+	| {
+			data: null;
+			error: Error;
+	  };
 
 export function parseJSON<T = any>(jsonString: string): ParseResult<T> {
-  try {
-    const parsedData = JSON.parse(jsonString) as T;
-    return {
-      data: parsedData,
-      error: null,
-    };
-  } catch (err) {
-    return {
-      data: null,
-      error: err instanceof SyntaxError ? err : new SyntaxError("Invalid JSON"),
-    };
-  }
+	try {
+		const parsedData = JSON.parse(jsonString) as T;
+		return {
+			data: parsedData,
+			error: null,
+		};
+	} catch (err) {
+		return {
+			data: null,
+			error:
+				err instanceof SyntaxError
+					? err
+					: new SyntaxError(
+							`Invalid JSON. ${
+								err instanceof Error ? err.message : String(err)
+							}`
+					  ),
+		};
+	}
 }
