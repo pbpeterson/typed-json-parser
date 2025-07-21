68010710f664f61adfa8fedcadb42103f6f4dc2b
Guilherme Saliba <salibagui19@gmail.com>
2025-07-20 21:57:22 -0300

    feat(wip): implement own parser

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,55 @@
+const JSON_WHITESPACE = new Set([" ", "\n", "\r", "\t"]);
+const JSON_SYNTAX = new Set(["{", "}", "[", "]", ",", ":"]);
+
+interface LexResult<T> {
+	value: T | null;
+	rest: string;
+}
+
 type ParseResult<T = any> =
-  | {
-      data: T;
-      error: null;
-    }
-  | {
-      data: null;
-      error: Error;
-    };
+	| {
+			data: T;
+			error: null;
+	  }
+	| {
+			data: null;
+			error: Error;
+	  };
 
 export function parseJSON<T = any>(jsonString: string): ParseResult<T> {
-  return {
-    data: "" as T,
-    error: null,
-  };
+	if (jsonString === null) {
+		return {
+			data: null,
+			error: { name: "NullError", message: "jsonString value is null" },
+		};
+	}
+
+	try {
+		const parsed = JSON.parse(jsonString); // TODO: Replace with own parse method
+		return {
+			data: parsed as T,
+			error: null,
+		};
+	} catch (err) {
+		return {
+			data: null,
+			error: new Error(),
+		};
+	}
+
+	return {
+		data: "" as T,
+		error: null,
+	};
+}
+
+function lex(input: string) {
+	const tokens: (string | number | boolean | null)[] = [];
+
+	while (input.length > 0) {
+		if (JSON_WHITESPACE.has(input[0])) {
+			input = input.slice(1);
+			continue;
+		}
+	}
 }
